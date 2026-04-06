594279f389bda1d46caab029fe52af79d95752f4
houtmanaj <jeromeaidoo5@gmail.com>
2026-04-06 12:37:30 +0000

    Initial commit: Assignment 1

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Jerome Aidoo";
+const age = 22;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
